6f48b693e0cc5dfac793d4c3b0d1066f1172b921
Josyi hub <yipenenye1@gmail.com>
2022-10-03 23:34:34 +0000

    Correct 1-multi_languages.js and add 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let argvLength = process.argv.length;
+if (argvLength === 2) {
+    console.log('No argument');
+} else if (argvLength ===3) {
+    console.log('Argument found');
+} else {
+    console.log('Argument found');
+}
+
+    
