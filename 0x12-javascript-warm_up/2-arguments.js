7606cc0b3872aacbb3ccbcc49fd30d4c6406bbbb
Josyi hub <yipenenye1@gmail.com>
2022-10-03 23:48:53 +0000

    Correct 1-multi_languages.js, 2-arguments.js and 3-value_argument.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,8 @@
 let argvLength = process.argv.length;
 if (argvLength === 2) {
     console.log('No argument');
-} else if (argvLength ===3) {
+} else if (argvLength === 3) {
     console.log('Argument found');
 } else {
     console.log('Argument found');
 }
-
-    
