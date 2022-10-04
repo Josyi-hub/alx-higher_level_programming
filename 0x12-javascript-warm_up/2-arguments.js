329d68d275195b4a019c18778b546d08093d54ff
Josyi hub <yipenenye1@gmail.com>
2022-10-04 00:05:30 +0000

    Correct 2-arguments.js       4-concat.js 1-multi_languages.js        3-value_argument.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-let argvLength = process.argv.length;
-if (argvLength === 2) {
-    console.log('No argument');
-} else if (argvLength === 3) {
-    console.log('Argument found');
+'use strict';
+let l = process.argv.length;
+if (l === 2) {
+  console.log('No argument');
+} else if (l === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+'use strict';
 let arg = process.argv[2];
 if (arg === undefined) {
   console.log('No argument');
