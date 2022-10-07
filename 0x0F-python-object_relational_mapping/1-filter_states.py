decdeb45e2c3f910b5eaeda64d7565a4ad362ad1
Josyi-hub <yipenenye1@gmail.com>
2022-10-07 08:55:37 -0700

    Correct 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa with a name
+starting with N
+"""
+
+
 import sys
 import MySQLdb
 
 username = sys.argv[1]
 passwd = sys.argv[2]
 dbname = sys.argv[3]
-conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                        passwd=passwd, db=dbname, charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    if row[1][0] == 'N':
-        print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
+    conn.close()
