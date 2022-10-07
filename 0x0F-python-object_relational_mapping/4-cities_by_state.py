f76d044f2e8805c0997e27d4506c990433dfd35b
Josyi-hub <yipenenye1@gmail.com>
2022-10-07 10:32:37 -0700

    Add 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if len(sys.argv) > 4 or len(sys.argv) < 4:
+    print("The script take three arguments: mysql username, password, dbname")
+    print("Exemple : ./4-cities_by_state.py root root hbtn_0e_4_usa")
+    exit()
+else:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd="root", db=dbname, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities, states where cities.state_id=states.id ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
